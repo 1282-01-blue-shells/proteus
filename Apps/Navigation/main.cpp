a3b349a142ab05eac8929741f46af4633bd2c443
Reina Quinn <edan.quinn@gmail.com>
2023-02-20 11:00:46 -0500

    Basic Navigation Program

---
+++ Apps/Navigation/main.cpp
@@ -0,0 +1,26 @@
+#include "proteos.hpp"
+
+#include "FEHMotor.h"
+#include "FEHUtility.h"
+
+void driveForward();
+
+FEHMotor leftMotor(FEHMotor::Motor0, 9);
+FEHMotor rightMotor(FEHMotor::Motor1, 9);
+
+int motorPower = 25;
+float time = 8.0;
+
+int main() {
+    registerIOVariable("motorPower", &motorPower);
+    registerIOVariable("time", &time);
+    registerIOFunction("driveForward()", &driveForward);
+}
+
+void driveForward() {
+    leftMotor.SetPercent(motorPower);
+    rightMotor.SetPercent(motorPower);
+    Sleep(time);
+    leftMotor.Stop();
+    rightMotor.Stop();
+}
