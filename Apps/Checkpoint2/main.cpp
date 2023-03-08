41f3ece9dd17fd3107ba2ae13ce84bb426949569
miedler6 <miedler.6@buckeyemail.osu.edu>
2023-03-08 17:09:31 -0500

    checkpoint2 stuff

--- Apps/Checkpoint2/main.cpp
+++ Apps/Checkpoint2/main.cpp
@@ -1,6 +1,120 @@
 #include "proteos.hpp"
 #include "navigation.hpp"
+#include "FEHMotor.h"
+#include "FEHUtility.h"
+//Author AK Miedler
+
+//Declaring Functions
+
+//Variables
+float bluethreshold=.4f;
+float redthreshold=.3f;
+float startingthreshold=2.f;
+
+AnalogInputPin lightSensor(FEHIO::P0_7);
+
+void waitForLight();
+void runcheckpoint2();
+void gotocoloredlight();
+void whatcolorisit();
+void comehome();
 
 int main() {
+    ProteOS::registerVariable("redthreshold", &redthreshold);
+    ProteOS::registerVariable("bluethreshold", &bluethreshold);
+
+    ProteOS::registerFunction("runcheckpoint2()", &runcheckpoint2);
+    ProteOS::registerFunction("waitForLight()",&waitForLight);
+    ProteOS::registerFunction("gotocoloredlight()",&gotocoloredlight);
+    ProteOS::registerFunction("whatcolorisit()",&whatcolorisit);
+    ProteOS::registerFunction("comehome()",&comehome);
     
+    ProteOS::run();
+
+}
+
+//running checkpoint
+void runcheckpoint2()
+{
+    waitForLight();
+    gotocoloredlight();
+    whatcolorisit();
+    comehome();
+}
+
+//function to keep bot stationary with no light and start moving when light turns on
+void waitForLight() {
+    Debugger::printLine(9, "Waiting for light...");
+
+    // wait for light
+    while (lightSensor.Value()>startingthreshold); // if no light do nothing
+
+
+    // Change font color to yellow
+    Debugger::setFontColor(0xFFFF00);
+
+    // Print funny message
+    Debugger::printWrap(10, "NOW AT LAST I SEEEE THE LIGHT");
+
+    // Reset font color
+    Debugger::setFontColor();
+}
+
+void gotocoloredlight(){
+    Debugger::printNextLine("starting"); //positions to be in line with ramp (eventually want RPS here)
+    Motors::drive(2);
+    Motors::turn(-20);
+    Motors::drive(2);
+    Motors::turn(-20);
+
+    Debugger::printNextLine("going forward"); //goes up ramp
+    Motors::drive(30);
+    Debugger::printNextLine("CLEARED THE RAMP");
+    Debugger::printNextLine("turning left"); //turns to be in line with light
+    Motors::turn(-30);
+
+    Debugger::printNextLine("going forward"); //goes to be on top of light
+    Motors::drive(20);
+
+    Motors::stop();
+    Debugger::sleep(2.f);
+    Debugger::printNextLine("determining light color...");
+}
+
+void whatcolorisit(){
+    // if light is blue
+    if (lightSensor.Value()>bluethreshold){
+        //backup into blue button
+        Debugger::printNextLine("LIGHT IS BLUE");
+        Motors::drive(-3); //back up 
+        Motors::turn(30); //turn to face kiosk
+        //Motors::turn(45); //position in front of blue button
+        Motors::drive(-6); //go until hit button (once add microswitches it will be until they are pressed)
+    }
+
+    //if light is red
+    if (lightSensor.Value()<redthreshold){
+        //backup into red button
+        Debugger::printNextLine("LIGHT IS RED"); 
+        Motors::drive(-3); //back up
+        Motors::turn(50); //turn so parallelish with kiosk
+        Motors::drive(2); //move forward a few inches
+        Motors::turn(-40); //position in front of red button
+        Motors::drive(6);//go until hit button
+    }
+
+}
+
+void comehome(){
+    //rps would be great here to get to same point moving on but whatever
+    Debugger::printNextLine("GARRRYYY COME HOME");
+    Motors::drive(-5); //move backward away from kiosk
+    Motors::turn(-90); //rotate forwards
+    Motors::drive(5); //move forward a little
+    Motors::turn(-90); //turn left
+    Motors::drive(3); //move forward a little
+    Motors::turn(90); //turn right
+    Motors::drive(15); //go down ramp
+    Motors::stop(); //cut motors
+    Debugger::printNextLine("GARRRYYYYY IS HOMEEEEE");
 }
