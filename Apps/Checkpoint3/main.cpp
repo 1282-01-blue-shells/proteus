8f164322508425640a569ec714ef1ff5e7f4435a
Reina Quinn <edan.quinn@gmail.com>
2023-03-22 19:54:10 -0400

    Checkpoint 3 Code

--- Apps/Checkpoint3/main.cpp
+++ Apps/Checkpoint3/main.cpp
@@ -1,31 +1,79 @@
 #include "proteos.hpp"
 #include "navigation.hpp"
 
+#include "math.h"
+
 #include "FEHLCD.h"
 #include "FEHServo.h"
+#include "FEHRPS.h"
 
+#define M_PI 3.1415926535f
+
+#define DEG_TO_RAD (M_PI / 180)
+#define RAD_TO_DEG (180 / M_PI)
 
 FEHServo mouthServo(FEHServo::Servo0);
 
+float leversX = 12, leversY = 23, leversH = 270;
+float leverSpacing = -3;
 
 void calibrateServo();
-void moveServo();
+void testServo();
+void initializeRPS();
+void displayPosition();
+void goToPosition();
+void calibrateQRCode();
+void testTurn();
+void testDrive();
+
+void flipLever(); // flips down then up, tester
+
+void flipLeverDown();
+void flipLeverUp();
+
+
 
 int main() {
     mouthServo.SetMin(500);
     mouthServo.SetMax(2390);
-    ProteOS::registerFunction("calibrateServo()", &calibrateServo);
-    ProteOS::registerFunction("moveServo()", &moveServo);
+    
+    mouthServo.SetDegree(90); //set to neutral position
+    
+    ProteOS::registerVariable("leversX", &leversX);
+    ProteOS::registerVariable("leversY", &leversY);
+    ProteOS::registerVariable("leversH", &leversH);
+    ProteOS::registerVariable("leverSpacing", &leverSpacing);
+    ProteOS::registerVariable("qrCodeX", &Motors::qrCodeX);
+    ProteOS::registerVariable("qrCodeY", &Motors::qrCodeY);
+    ProteOS::registerVariable("qrCodeA", &Motors::qrCodeA);
+    
+    //ProteOS::registerFunction("calibrateServo()", &calibrateServo);
+    //ProteOS::registerFunction("testServo()", &testServo);
+    ProteOS::registerFunction("initializeRPS()", &initializeRPS);
+    ProteOS::registerFunction("calibrateQRCode()", &calibrateQRCode);
+    ProteOS::registerFunction("displayPosition()", &displayPosition);
+    ProteOS::registerFunction("goToPosition()", &goToPosition);
+    ProteOS::registerFunction("flipLever()", &flipLever);
+    //ProteOS::registerFunction("flipLeverDown()", &flipLeverDown);
+    //ProteOS::registerFunction("flipLeverUp()", &flipLeverUp);
+    ProteOS::registerFunction("testTurn()", &testTurn);
+    ProteOS::registerFunction("testDrive()", &testDrive);
 
     ProteOS::run();
 }
-
+//STILL HAVE TO DO 
+// 1) Move at the light
+// 2) Once get the signal of which fuel pump it is move based off that
 
 void calibrateServo() {
     mouthServo.TouchCalibrate();
 }
 
-void moveServo() {
+void calibrateQRCode() {
+    Motors::calibrateQRCode();
+}
+
+void testServo() {
     float servoAngle = 0;
     Debugger::printWrap(4, "Touch the left side of the screen to move the servo left, and the right side to move it right");
     while (true) {
@@ -44,3 +92,102 @@ void moveServo() {
         Debugger::sleep(0.05f);
     }
 }
+
+void initializeRPS() {
+    RPS.InitializeTouchMenu();
+}
+
+void displayPosition() {
+    while (true) {
+        float x, y, h;
+        Motors::getCurrentPos(&x, &y, &h);
+        Debugger::printLine(2, "X: %f", x);
+        Debugger::printLine(3, "Y: %f", y);
+        Debugger::printLine(4, "H: %f", h);
+    }
+}
+
+void goToPosition() {
+    int correctLever = RPS.GetIceCream(); //getting the correct lever position
+    float correctLeverPosition;
+    if (correctLever == 0) {
+        Debugger::printNextLine("Going to left lever");
+        correctLeverPosition = leversX;
+    } else if (correctLever == 1) {
+        Debugger::printNextLine("Going to middle lever");
+        correctLeverPosition = leversX + 1*leverSpacing;
+    } else if (correctLever == 2) {
+        Debugger::printNextLine("Going to right lever");
+        correctLeverPosition = leversX + 2*leverSpacing;
+    }
+    Motors::driveTo(correctLeverPosition, leversY, leversH);
+}
+
+void flipLever() {
+    flipLeverDown();
+    Debugger::sleep(5);
+    flipLeverUp();
+}
+
+void flipLeverDown() {
+    mouthServo.SetDegree(55); //position the lever to be above the lever
+    Debugger::sleep(0.5f);
+    Debugger::printNextLine("AT 114 Degrees. Now Moving Forward");
+
+    Motors::drive(2); //drive forward to the lever
+
+    mouthServo.SetDegree(90); //bop the lever down
+    Debugger::sleep(0.5f);
+
+    // Back up again
+    Motors::drive(-2);
+}
+
+void flipLeverUp() {
+    mouthServo.SetDegree(125); //position lever underneath fuel pump
+    Debugger::sleep(0.5f);
+
+    Motors::drive(2); //drive forward again 
+    mouthServo.SetDegree(90); //bop lever up 
+
+    Motors::drive(-2);
+}
+
+
+void testTurn() {
+    float x, y, h;
+    Motors::getCurrentPos(&x, &y, &h);
+
+    float targetX = x;
+    float targetY = y;
+    float targetH = h + 90;
+    if (targetH > 360) targetH -= 360;
+
+    Motors::turn(-90);
+
+    Motors::getCurrentPos(&x, &y, &h);
+
+    Debugger::printNextLine("X: exp %f act %f", targetX, x);
+    Debugger::printNextLine("Y: exp %f act %f", targetY, y);
+    Debugger::printNextLine("H: exp %f act %f", targetH, h);
+}
+
+void testDrive() {
+    float x, y, h;
+    Motors::getCurrentPos(&x, &y, &h);
+
+    float a = h * DEG_TO_RAD;
+    float targetX = x + cos(a)*6;
+    float targetY = y + sin(a)*6;
+    float targetH = h;
+
+    Motors::drive(6);
+
+    Motors::getCurrentPos(&x, &y, &h);
+
+    Debugger::printNextLine("X: exp %f act %f", targetX, x);
+    Debugger::printNextLine("Y: exp %f act %f", targetY, y);
+    Debugger::printNextLine("H: exp %f act %f", targetH, h);
+}
+
+
